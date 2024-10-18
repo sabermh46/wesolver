cf6dec9f8f8a8a8bd35ac5eae563902a2daf69f6
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2024-10-19 02:54:09 +0600

    up

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../assets/logo.png"
+
+const Header: React.FC = () => {
+    return <header>
+        <nav>
+            
+        </nav>
+    </header>;
+  };
+  
+  export default Header;
