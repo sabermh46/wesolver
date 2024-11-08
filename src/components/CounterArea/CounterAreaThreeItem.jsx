43e42c0eca11e6c7047c2fcf6ff97cf838da8227
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2024-11-08 16:37:20 +0600

    Counter Animated

--- src/components/CounterArea/CounterAreaThreeItem.jsx
+++ src/components/CounterArea/CounterAreaThreeItem.jsx
@@ -1,22 +1,30 @@
 import React from "react";
-import Odometer from "react-odometerjs";
-
+import { useInView } from "react-intersection-observer";
+import '../../styles/odom.css'
+import CountUp from "react-countup";
 const CounterAreaThreeItem = (props) => {
+
+  const {ref, inView} = useInView({
+    triggerOnce: true,
+    threshold: 0.1
+
+  })
+
   return (
     <>
-      <div className="counter-item-three">
+      <div ref={ref} className="counter-item-three"
+      style={{
+        transition: '0.5s ease',
+        transform: `scale(${inView? 1 : 0.3})`
+      }}
+      >
         <div className="icon">
           <img src={props.item.src} alt="" />
         </div>
         <div className="content">
           <h2 className="count">
-            <span className="odometer" data-count="210">
-              <Odometer
-                value={props.amount}
-                format="(,ddd).dd"
-                duration={1000}
-                animation={"count"}
-              />
+            <span className="odometer">
+              <CountUp duration={2} end={inView ? props.amount : 0} />
             </span>
             +
           </h2>
