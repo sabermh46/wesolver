692ff39a306eeb5a78291e4212bc0042ec17d901
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2024-11-08 12:37:37 +0600

    ABout me animation

--- src/components/DeveloperArea/DeveloperExperienceArea.jsx
+++ src/components/DeveloperArea/DeveloperExperienceArea.jsx
@@ -1,16 +1,41 @@
 import React from "react";
+import { useInView } from "react-intersection-observer";
 
 const DeveloperExperienceArea = () => {
+
+  const settingIO = {
+    triggerOnce: false,
+    threshold: 0.5
+  }
+  const { ref: ref1, inView: iV1} = useInView(settingIO)
+  const { ref: ref2, inView: iV2} = useInView(settingIO)
+
   return (
     <div className="developr-img ">
-      <img src="/img/images/developer_img.png" alt="" />
-      <div className="work-experience-wrap">
+      <img style={{
+                transition: '0.5s ease',
+                transform: `translateY(${(iV1 || iV2 ) ? 0 : -50}px)`,
+                opacity: (iV1 || iV2 ) ? 1 : 0.3
+                
+              }}
+       src="/img/images/developer_img.png" alt="" />
+      <div ref={ref1} className="work-experience-wrap"
+      style={{
+        transition: '0.5s ease',
+        transform: `translateX(${iV1? 0 : -50}px)`
+      }}
+      >
         <h2 className="count ">
           <span className="odometer" data-count="18"></span>5 +
         </h2>
         <p>Years Of Experience</p>
       </div>
-      <div className="project-completed-wrap">
+      <div ref={ref2} className="project-completed-wrap"
+      style={{
+        transition: '0.5s ease',
+        transform: `translateX(${iV2? 0 : 50}px)`
+      }}
+      >
         <h2 className="count">
           <span className="odometer" data-count="100">50</span>+
         </h2>
